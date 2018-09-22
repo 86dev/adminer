a054e6e321e73b87f6222491c440ba29a42af2bc
jtisseau <jonathan.tisseau@86dev.fr>
2018-09-22 02:05:48 +0200

    initial (4.6.3)

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+function adminer_object() {
+	// required to run any plugin
+	include_once "./plugins/plugin.php";
+
+	// autoloader
+	foreach (glob("./plugins/*.php") as $filename) {
+		include_once "./$filename";
+	}
+
+	$plugins = array(
+		// specify enabled plugins here
+		new AdminerBootstrapLike,
+		new AdminerEditForeign,
+		new AdminerForeignSystem,
+		new AdminerFrames,
+		new AdminerLoginSqlite,
+		new AdminerFloatThead,
+		new AdminerEditCalendar,
+		new AdminerDumpJson,
+		new AdminerDumpAlter,
+		new AdminerJsonColumn,
+		new AdminerTablesFilter,
+		//new AdminerLoginServers($servers),
+	);
+
+	/* It is possible to combine customization and plugins:
+	class AdminerCustomization extends AdminerPlugin {
+	}
+	return new AdminerCustomization($plugins);
+	*/
+
+	return new AdminerPlugin($plugins);
+}
+
+// include original Adminer or Adminer Editor
+include "./adminer-4.6.3.php";
+?>
