2713a879ee6b860e5e7dcfcb0d77bd2a477ab8b9
jonathantisseau <jonathan.tisseau@86dev.fr>
2022-12-12 10:15:05 +0100

    4.8.1

--- assets/scripts.js
+++ assets/scripts.js
@@ -7,6 +7,7 @@
  * @license https://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0
  * @license https://www.gnu.org/licenses/gpl-2.0.html GNU General Public License, version 2 (one or other)
  */
+/*
 (function(window) {
 	window.addEventListener('load', function() {
 		adminerDesign.init();
@@ -241,3 +242,4 @@
 
 	};
 })(window);
+*/
--- index.php
+++ index.php
@@ -1,50 +1,50 @@
 <?php
 
 $PLUGINS_LIST = [
-	// (object)[
-	// 	'file' => './plugins/tables-filter.php',
-	// 	'class' => 'AdminerTablesFilter',
-	// ],
-	// (object)[
-	// 	'file' => './plugins/bootstrap-like.php',
-	// 	'class' => 'AdminerBootstrapLike',
-	// ],
-	// (object)[
-	// 	'file' => './plugins/edit-foreign.php',
-	// 	'class' => 'AdminerEditForeign',
-	// ],
-	// (object)[
-	// 	'file' => './plugins/foreign-system.php',
-	// 	'class' => 'AdminerForeignSystem',
-	// ],
-	// (object)[
-	// 	'file' => './plugins/login-sqlite.php',
-	// 	'class' => 'AdminerLoginSqlite',
-	// ],
-	// (object)[
-	// 	'file' => './plugins/floatThead.php',
-	// 	'class' => 'AdminerFloatThead',
-	// ],
-	// (object)[
-	// 	'file' => './plugins/edit-calendar.php',
-	// 	'class' => 'AdminerEditCalendar',
-	// ],
-	// (object)[
-	// 	'file' => './plugins/dump-json.php',
-	// 	'class' => 'AdminerDumpJson',
-	// ],
-	// (object)[
-	// 	'file' => './plugins/dump-yaml.php',
-	// 	'class' => 'AdminerDumpYaml',
-	// ],
-	// (object)[
-	// 	'file' => './plugins/dump-alter.php',
-	// 	'class' => 'AdminerDumpAlter',
-	// ],
-	// (object)[
-	// 	'file' => './plugins/json-column.php',
-	// 	'class' => 'AdminerJsonColumn',
-	// ],
+	(object)[
+		'file' => './plugins/tables-filter.php',
+		'class' => 'AdminerTablesFilter',
+	],
+	(object)[
+		'file' => './plugins/bootstrap-like.php',
+		'class' => 'AdminerBootstrapLike',
+	],
+	(object)[
+		'file' => './plugins/edit-foreign.php',
+		'class' => 'AdminerEditForeign',
+	],
+	(object)[
+		'file' => './plugins/foreign-system.php',
+		'class' => 'AdminerForeignSystem',
+	],
+	(object)[
+		'file' => './plugins/login-sqlite.php',
+		'class' => 'AdminerLoginSqlite',
+	],
+	(object)[
+		'file' => './plugins/floatThead.php',
+		'class' => 'AdminerFloatThead',
+	],
+	(object)[
+		'file' => './plugins/edit-calendar.php',
+		'class' => 'AdminerEditCalendar',
+	],
+	(object)[
+		'file' => './plugins/dump-json.php',
+		'class' => 'AdminerDumpJson',
+	],
+	(object)[
+		'file' => './plugins/dump-yaml.php',
+		'class' => 'AdminerDumpYaml',
+	],
+	(object)[
+		'file' => './plugins/dump-alter.php',
+		'class' => 'AdminerDumpAlter',
+	],
+	(object)[
+		'file' => './plugins/json-column.php',
+		'class' => 'AdminerJsonColumn',
+	],
 ];
 
 function adminer_object() {
@@ -53,12 +53,13 @@ function adminer_object() {
 	global $PLUGINS_LIST;
 	foreach ($PLUGINS_LIST as $plugin) {
 		require_once $plugin->file;
-		$plugins[] = new ($plugin->class)();
+		$plugins[] = new $plugin->class();
 	}
 
 	return new AdminerPlugin($plugins);
 }
 
 // include original Adminer or Adminer Editor
+error_reporting(0);
 include "./adminer-4.8.1.php";
 ?>
